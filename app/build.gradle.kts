d1143ed18b8398f00df0436c7115e7a9639101d6
“enmanuel” <enmanueldelgado52@gmail.com>
2024-04-21 23:51:47 -0400

    without baseline profile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("androidx.baselineprofile")
+//    id("androidx.baselineprofile")
 }
 
 android {
@@ -77,9 +77,9 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    "baselineProfile"(project(":baselineprofile"))
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
-    implementation("androidx.profileinstaller:profileinstaller:1.3.1")
+//    "baselineProfile"(project(":baselineprofile"))
+//    implementation("androidx.profileinstaller:profileinstaller:1.3.1")
 }
