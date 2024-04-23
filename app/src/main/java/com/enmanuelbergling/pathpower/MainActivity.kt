d552d95945d7229ba5f686ae48d13995c13bc18f
“enmanuel” <enmanueldelgado52@gmail.com>
2024-04-22 22:17:44 -0400

    adding a better example

--- app/src/main/java/com/enmanuelbergling/pathpower/MainActivity.kt
+++ app/src/main/java/com/enmanuelbergling/pathpower/MainActivity.kt
@@ -8,7 +8,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.enmanuelbergling.pathpower.ui.animation.liquid.LiquidCircles
+import com.enmanuelbergling.pathpower.ui.animation.liquid.MoreIcons
+import com.enmanuelbergling.pathpower.ui.animation.liquid.MoreIconsContainer
 import com.enmanuelbergling.pathpower.ui.theme.PathPowerTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                        LiquidCircles()
+                        MoreIconsContainer()
                     }
                 }
             }
--- app/src/main/java/com/enmanuelbergling/pathpower/ui/animation/liquid/LiquidAnimation.kt
+++ app/src/main/java/com/enmanuelbergling/pathpower/ui/animation/liquid/LiquidAnimation.kt
@@ -5,22 +5,30 @@ import android.graphics.ColorMatrixColorFilter
 import android.graphics.RenderEffect
 import android.graphics.Shader
 import android.os.Build
+import androidx.annotation.FloatRange
 import androidx.annotation.RequiresApi
+import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.Build
+import androidx.compose.material.icons.rounded.Favorite
 import androidx.compose.material3.Button
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -29,20 +37,24 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.asComposeRenderEffect
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
+import kotlin.math.cos
+import kotlin.math.sin
+import androidx.compose.ui.graphics.RenderEffect as ComposeRenderEffect
 
 @RequiresApi(Build.VERSION_CODES.S)
-@Preview
 @Composable
 fun LiquidCircles() {
     val primaryColor = MaterialTheme.colorScheme.primary
 
-    val liquidEffect =
-        rememberLiquidEffect()
+    val liquidEffect = rememberLiquidEffect()
 
     /*Box(
         modifier = Modifier
@@ -64,7 +76,8 @@ fun LiquidCircles() {
 
     val animationProgress by animateFloatAsState(
         targetValue = if (isExpanded) 1f else 0f,
-        animationSpec = tween(2_000), label = "animation progress"
+        animationSpec = tween(2_000),
+        label = "animation progress"
     )
 
     Box(
@@ -87,18 +100,16 @@ fun LiquidCircles() {
 
 @Composable
 private fun BoxWithIconGroup(
-    liquidEffect: androidx.compose.ui.graphics.RenderEffect?,
+    liquidEffect: ComposeRenderEffect?,
     animationProgress: Float,
     heightPx: Float,
     primaryColor: Color,
 ) {
-    Box(
-        modifier = Modifier
-            .fillMaxHeight()
-            .graphicsLayer {
-                renderEffect = liquidEffect
-            }
-    ) {
+    Box(modifier = Modifier
+        .fillMaxHeight()
+        .graphicsLayer {
+            renderEffect = liquidEffect
+        }) {
 
         BoxWithIcon(animationProgress, heightPx, primaryColor, 0f)
 
@@ -121,8 +132,7 @@ private fun BoxWithIcon(
         }
         .size(150.dp)
         .clip(CircleShape)
-        .background(primaryColor)
-    ) {
+        .background(primaryColor)) {
         Icon(
             imageVector = Icons.Rounded.Build,
             contentDescription = "some icon",
@@ -136,10 +146,9 @@ private fun BoxWithIcon(
 
 @RequiresApi(Build.VERSION_CODES.S)
 @Composable
-private fun rememberLiquidEffect(): androidx.compose.ui.graphics.RenderEffect {
+private fun rememberLiquidEffect(): ComposeRenderEffect {
     val radius = 60f
-    val blurEffect = RenderEffect
-        .createBlurEffect(radius, radius, Shader.TileMode.DECAL)
+    val blurEffect = RenderEffect.createBlurEffect(radius, radius, Shader.TileMode.DECAL)
 
     val matrixArray = floatArrayOf(
         1f, 0f, 0f, 0f, 0f, //Red
@@ -158,4 +167,152 @@ private fun rememberLiquidEffect(): androidx.compose.ui.graphics.RenderEffect {
         RenderEffect.createChainEffect(colorFilter, blurEffect).asComposeRenderEffect()
 
     return remember { liquidEffect }
+}
+
+@RequiresApi(Build.VERSION_CODES.S)
+@Composable
+fun MoreIconsContainer() {
+    val liquidEffect = rememberLiquidEffect()
+
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+
+    val animationProgress = remember {
+        Animatable(0f)
+    }
+
+    LaunchedEffect(key1 = expanded) {
+        val animationSpec =
+            tween<Float>(2_500)// spring<Float>(Spring.DampingRatioMediumBouncy, Spring.StiffnessLow)
+
+        if (expanded) {
+            animationProgress.animateTo(targetValue = 1f, animationSpec = animationSpec)
+        } else {
+            animationProgress.animateTo(targetValue = 0f, animationSpec = animationSpec)
+        }
+    }
+
+
+    Box {
+        MoreIcons(animationProgress = animationProgress.value, renderEffect = liquidEffect) {}
+        MoreIcons(animationProgress = animationProgress.value, renderEffect = null) {
+            expanded = !expanded
+        }
+    }
+}
+
+@Composable
+fun MoreIcons(animationProgress: Float, renderEffect: ComposeRenderEffect?, onToggle: () -> Unit) {
+    //Just icon should overlap the liquid animation
+    val containerColor =
+        if (renderEffect != null) MaterialTheme.colorScheme.primaryContainer else Color.Transparent
+
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .graphicsLayer {
+            this.renderEffect = renderEffect
+        }) {
+        val count = 4
+        val maxAngle = 180f
+        val angleStep = maxAngle / (count - 1)
+        repeat(count) { index ->
+            val angle = index * angleStep
+
+            val offset = getOffsetAround(angle = angle, radius = 90.dp)
+
+            LiquidFAB(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .graphicsLayer {
+                        val maximumValue = (index + 1f) / count
+                        val itemProgress =
+                            animationProgress.coerceAtMost(maximumValue) / maximumValue
+                        translationY = offset.y.toPx() * itemProgress
+                        translationX = offset.x.toPx() * itemProgress
+                        rotationZ = -maxAngle + (maxAngle * itemProgress)
+                    },
+                containerColor = containerColor,
+                imageVector = if (renderEffect == null) Icons.Rounded.Favorite else null
+            )
+        }
+
+
+        LiquidFAB(
+            onClick = onToggle,
+            modifier = Modifier
+                .align(Alignment.Center)
+                .graphicsLayer {
+                    rotationZ = 45f * animationProgress
+                },
+            containerColor = containerColor,
+            imageVector = if (renderEffect == null) Icons.Rounded.Add else null
+        )
+    }
+}
+
+@Composable
+private fun LiquidFAB(
+    modifier: Modifier = Modifier,
+    imageVector: ImageVector? = null,
+    shape: Shape = CircleShape,
+    containerColor: Color = MaterialTheme.colorScheme.primaryContainer,
+    contentColor: Color = MaterialTheme.colorScheme.onPrimaryContainer,
+    onClick: () -> Unit,
+) {
+    FloatingActionButton(
+        onClick = onClick,
+        modifier = modifier,
+        shape = shape,
+        elevation = FloatingActionButtonDefaults.elevation(0.dp, 0.dp, 0.dp, 0.dp),
+        containerColor = containerColor,
+        contentColor = contentColor,
+    ) {
+        if (imageVector != null) {
+            Icon(imageVector = imageVector, contentDescription = "favorite")
+        }
+    }
+}
+
+/**
+ * Fully based on Trigonometry, starting on 9:00 clockwise
+ * @param radius stands for hypotenuse
+ * @param angle stands for alpha
+ * */
+@Composable
+fun getOffsetAround(
+    @FloatRange(
+        from = 0.0,
+        to = 360.0,
+        fromInclusive = true,
+        toInclusive = false,
+    ) angle: Float,
+    radius: Dp,
+): DpOffset {
+    val newAngle = angle % 90
+    val radians = Math.toRadians(newAngle.toDouble()).toFloat()
+
+    val oppositeSide = radius * sin(radians) //sin
+    val adjacentSide = radius * cos(radians) //cos
+
+    return when (angle) {
+        in 0f..<90f -> DpOffset(
+            x = -adjacentSide, y = -oppositeSide
+        )
+
+        in 90f..<180f -> DpOffset(
+            x = oppositeSide, y = -adjacentSide
+        )
+
+        in 180f..<270f -> DpOffset(
+            x = adjacentSide, y = oppositeSide
+        )
+
+        in 270f..<360f -> DpOffset(
+            x = -oppositeSide, y = adjacentSide
+        )
+
+        else -> DpOffset.Zero
+    }
 }
