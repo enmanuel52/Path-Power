8b03ba53f24d947cc01a4849b2a4957946487339
Manel Martos <mmartos@degirona.info>
2024-10-18 09:40:57 +0200

    Added new wave types based on AGSL

---
+++ path_power/src/main/java/com/enmanuelbergling/path_power/ui/shaders/PlasmaShader.kt
@@ -0,0 +1,30 @@
+package com.enmanuelbergling.path_power.ui.shaders
+
+import android.graphics.RuntimeShader
+import org.intellij.lang.annotations.Language
+
+@Language("AGSL")
+private val shader = """
+    uniform float2 resolution;
+    uniform float time;
+    uniform float dimmer;
+    
+    half4 main(vec2 fragCoord) { 
+        vec2 uv = fragCoord / resolution.xy;
+        vec3 colGradient = 0.5
+                + 0.9 * sin(-time + (uv.x * uv.x) + (uv.y * uv.y) + vec3(8, 1, 3))
+                      * cos(time + (uv.y * uv.x) + (uv.y * uv.y) + vec3(5, 1, 6))
+                + 0.5 * cos(time + (uv.y * uv.x) + (uv.y * uv.y) + vec3(1, 2, 3))
+                      * cos(time + (uv.y * uv.x) + (uv.y * uv.y) + vec3(13, 5, 8))
+                      * cos(time + (uv.y * uv.x) + (uv.y * uv.y) + vec3(1, 2, 1)) 
+                + 0.5 * cos(time + (uv.y * uv.x) + (uv.y * uv.y) + vec3(1, 2, 3));
+        return half4(colGradient * dimmer, dimmer);
+    }    
+""".trimIndent()
+
+fun plasmaShader(
+    dimmer: Float = 1f,
+): RuntimeShader =
+    RuntimeShader(shader).apply {
+        setFloatUniform("dimmer", dimmer)
+    }
