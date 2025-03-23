08b9da08131411e7465e31b823a10d6a0aabdc42
enmanuel <enmanueldelgado52@gmail.com>
2025-03-23 12:57:31 -0400

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.enmanuelbergling.pathpower"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.enmanuelbergling.pathpower"
         minSdk = 33
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
