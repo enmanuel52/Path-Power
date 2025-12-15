29fa29de08da322d25295480f53badd5189933ce
enmanuel <enmanueldelgado52@gmail.com>
2025-12-15 17:57:19 -0300

    update maven plugin

--- path_power/build.gradle.kts
+++ path_power/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
     alias(libs.plugins.kotlin.compose.compiler)
-    `maven-publish`
+    id("maven-publish")
 //    id("androidx.baselineprofile")
 }
 
